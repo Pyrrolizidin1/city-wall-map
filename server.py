dd32f2aef33d453f8ddbc1812edbcad2c9339ec9
Pyrrolizidin1 <Pyrrolizidin@163.com>
2026-07-09 13:44:58 +0800

    初始提交：古城墙地图数据

---
+++ server.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import sys
+
+class CORSRequestHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', '*')
+        self.send_header('Access-Control-Allow-Headers', '*')
+        super().end_headers()
+    
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.end_headers()
+
+if __name__ == '__main__':
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else 8080
+    print(f"Server running on http://localhost:{port}")
+    httpd = HTTPServer(('localhost', port), CORSRequestHandler)
+    httpd.serve_forever()
